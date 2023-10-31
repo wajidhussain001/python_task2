dccab7ad10964f7174de9732497001cd1086186f
wajidhussain001 <147056326+wajidhussain001@users.noreply.github.com>
2023-10-31 18:02:30 +0530

    Add files via upload

---
+++ python_task2/python1.py
@@ -0,0 +1,68 @@
+num = 23
+alphabet = "Wajid"
+boo = True
+num1 = 11.00
+print(type(num))
+print(type(alphabet))
+print(type(num1))
+print(type(bool))
+
+# Casting
+number = 20
+temp1 = float(number)
+print(temp1)
+
+Wajid = "3"  # string
+print(Wajid)
+
+String = "Wajid"
+print(String)
+
+string = "Wajid"
+print(string[4])
+
+string1 = "Wajid Hussain"
+print(string1[6])
+
+String1 = "Wajid"
+for temp in String:
+    print(temp)
+
+Value = "Wajid"
+print(len(Value))
+
+Value1 = "hello world"
+print("world" in Value)
+
+Value2 = "Wajid Hussain"
+print(Value2[0:5])
+
+Value3 = "Hussain"
+
+print(Value.upper())
+print(Value.lower())
+
+temp2 = Value + Value3
+print(temp2)
+print(Value.startswith("W"))
+print(Value3.endswith("n"))
+
+Value4 = 2
+Value5 = 5
+
+print(Value5 > Value4)
+
+# If Else
+Value6 = 44
+Value7 = 64
+
+if Value6 > Value7:
+    print("Wajid")
+else:
+    print("error")
+
+Value8 = 44
+Value9 = 64
+
+if Value9 > Value8:
+    print("Wajid")
